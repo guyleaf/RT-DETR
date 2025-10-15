fix(rtdetrv2): Fix incorrect dtype during loss calculation if training with the AMP (#12)
* fix(rtdetrv2/train): Move criterion to the context of autocast to solve the possiblity of exception

* refactor(rtdetrv2/train): Pass device.type to the argument instead of str(device)

rtdetrv2_pytorch/rtdetrv2/solver/det_engine.py
@@ -52,10 +52,8 @@ def train_one_epoch(
         metas = dict(epoch=epoch, step=i, global_step=global_step)
 
         if scaler is not None:
-            with torch.autocast(device_type=str(device), cache_enabled=True):
+            with torch.autocast(device_type=device.type, cache_enabled=True):
                 outputs = model(samples, targets=targets)
-
-            with torch.autocast(device_type=str(device), enabled=False):
                 loss_dict = criterion(outputs, targets, **metas)
 
             loss = sum(loss_dict.values())