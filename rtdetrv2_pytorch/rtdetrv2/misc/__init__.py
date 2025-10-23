9821b03e50dbf24883d0a31a2aff4d8e3a8dcdd0
Leaf Ying <43506966+guyleaf@users.noreply.github.com>
2025-10-23 15:58:26 +0800

    feat(rtdetrv2)!: Support env setup for external registration and other loadings & Remove old preloading method (#13)
    * feat(rtdetrv2): Support setup env for external registeration and other loadings
    
    * refactor(rtdetrv2)!: Remove old preloading method

--- rtdetrv2_pytorch/rtdetrv2/misc/__init__.py
+++ rtdetrv2_pytorch/rtdetrv2/misc/__init__.py
@@ -1,7 +1,7 @@
 """Copyright(c) 2023 lyuwenyu. All Rights Reserved."""
 
 from .dist_utils import setup_print, setup_seed
-from .loader import *
+from .env import setup_custom_environment, setup_environment
 from .logger import *
 from .profiler_utils import stats
 from .visualizer import *
